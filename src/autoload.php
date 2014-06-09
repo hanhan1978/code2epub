e0438fc7cf7ee602cb7524ae204ee5ac5025ae3f
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-06-10 01:42:37 +0900

    added autoloader.php

---
+++ src/autoload.php
@@ -0,0 +1,20 @@
+<?php
+
+spl_autoload_register("loadLibrary");
+
+
+define("DS", "/");
+
+/*
+ * Autoloader for phpepub
+ * @param $className  Name of class to be loaded
+ */
+function loadLibrary($className) {
+    $it = new RecursiveDirectoryIterator(dirname(__FILE__));
+    $it = new RecursiveIteratorIterator($it);
+ 
+    foreach ($it as $fileinfo) { 
+        if ($fileinfo->isFile() && $className.".php" === basename($fileinfo->getPathname()))
+            require_once $fileinfo->getPathname();
+    }
+}

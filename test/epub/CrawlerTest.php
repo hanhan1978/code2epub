77c31422e9c258a4254ecaef91d936fde4dba5d3
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-06-20 10:57:58 +0900

    remove space

--- test/epub/CrawlerTest.php
+++ test/epub/CrawlerTest.php
@@ -9,6 +9,6 @@ class CrawlerTest extends PHPUnit_Framework_TestCase{
         $this->assertEquals(count($sample1->getChildren()), 4);
         $sample2 = Crawler::crawl(TEST_ROOT."/res/sampleOnlyOneFile/fuga.txt");
         $this->assertEquals(count($sample2), 1);
-
     }
+
 }

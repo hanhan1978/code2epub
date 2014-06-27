50322e80a0041aaea239629ad7c73573634cdfe5
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-06-27 14:22:38 +0900

    fix test

--- test/phpepub/entity/file/FileEntryTest.php
+++ test/phpepub/entity/file/FileEntryTest.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__)."/../../../testHelper.php";
 class FileEntryTest extends PHPUnit_Framework_TestCase{
 
     public function testToHtml(){
-        $entry1 = FileFactory::createFileEntry(TEST_ROOT."/res/sampleOnlyOneFile/fuga.txt");
+        $entry1 = FileEntry(TEST_ROOT."/res/sampleOnlyOneFile/fuga.txt");
         $this->assertEquals("This is fuga file.", rtrim($entry1->toHtml()));
     }
 

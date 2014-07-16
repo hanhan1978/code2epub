660caa11b922eff1dd472571e51032e835e1640f
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-07-16 11:56:38 +0900

    fix for travis

--- src/phpepub/crawler/FileCrawler.php
+++ src/phpepub/crawler/FileCrawler.php
@@ -14,17 +14,28 @@ class FileCrawler extends Crawler{
         if(!is_dir($dirname))
             return new FileEntry($dirname, $parentName);
         else
-            $d=dir($dirname);
+            $entries = $this->getEntries($dirname);
 
         $box = new DirectoryEntry($dirname, $parentName); 
+        foreach($entries as $entry){
+            if(is_dir($entry))
+                $box->add(self::crawlImple($entry, $box->getName())); 
+            else
+                $box->add(new FileEntry($entry, $box->getName()));
+        }
+        return $box;
+    }
+
+
+    private function getEntries($dirname){
+        $list = array();
+        $d=dir($dirname);
         while($entry = $d->read()){
             if(preg_match("/^\.+$/", $entry)) continue;
-            if(is_dir($dirname.DS.$entry))
-                $box->add(self::crawlImple($dirname.DS.$entry, $box->getName())); 
-            else
-                $box->add(new FileEntry($dirname.DS.$entry, $box->getName()));
+            $list[] = $dirname.DS.$entry;
         }
         $d->close();
-        return $box;
+        sort($list);
+        return $list;
     }
 }

7122f3c76d4fa85e4bd902c831efaebcd6a25d92
hanhan1978 <ryo.tomidokoro@gmail.com>
2014-07-16 12:08:50 +0900

    remove unnecessary dump

--- test/phpepub/crawler/FileCrawlerTest.php
+++ test/phpepub/crawler/FileCrawlerTest.php
@@ -14,11 +14,9 @@ class FileCrawlerTest extends PHPUnit_Framework_TestCase{
 
     function testCrawl(){
         $sample1 = $this->nestFileObj->crawl();
-        $sample1->dump();
         $this->assertEquals(basename($sample1->getChildren()[2]->getChildren()[2]->getChildren()[1]->getPath()), 'fuga4-2.txt');
         $this->assertEquals(count($sample1->getChildren()), 4);
         $sample2 = $this->singleFileObj->crawl(); 
-        $sample2->dump();
         $this->assertEquals(count($sample2), 1);
     }
 
